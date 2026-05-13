64406021c4f05a4f0bbfc04b51182a52035af9e9
meltingseaweed <conor.seo@gmail.com>
2026-05-13 18:24:36 +0900

    Build: Add base framework for index and new for both Users and Posts
    Base html layout and forms have been created but
    will need to be updated. The form for Post will
    not be created unless a user is specified

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,42 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
   end
 
   def show
   end
 
   def create
-  end
-
-  def new
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
+
+  private
+    def post_params
+      params.expect(post: [ :title, :body, :closing ])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
@@ -7,5 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+  def user_params
+    params.expect(user: [ :username, :email, :password ])
   end
 end
