1f9717e6e515d55f854c891403a5a8312b81c746
meltingseaweed <conor.seo@gmail.com>
2026-05-13 17:01:17 +0900

    Build: Add controllers
    Add controllers and connect with routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,24 +1,3 @@
 class Post < ApplicationRecord
   belongs_to :user
-
-  def index
-  end
-
-  def show
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
