ad6c4890d680a0ccba9f31bc9038406e6375fdf3
meltingseaweed <conor.seo@gmail.com>
2026-05-14 14:12:41 +0900

    Feat: Remove uneeded files. At username to user which works with devise
    Previously username was not stored with the devise
    user, so change controller to store it properly
    and integrate it into the website.
    Remove uneeded files.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
-  # validates :title, :body, presence: true
+  validates :username, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
   resources :posts, only: [ :new, :create, :index ]
   # resources :users
 
